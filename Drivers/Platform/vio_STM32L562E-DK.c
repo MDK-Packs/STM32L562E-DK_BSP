7822d623534e25542d3932d1202a8a0cb18ce782
Ziga Mahne <ziga.mahne@arm.com>
2020-06-19 11:21:11 +0200

    Minor fixes

--- Drivers/Platform/vio_STM32L562E-DK.c
+++ Drivers/Platform/vio_STM32L562E-DK.c
@@ -433,7 +433,7 @@ void vioSetSignal (uint32_t mask, uint32_t signal) {
 #if !defined CMSIS_VOUT
   // Output signals to LEDs
   if ((mask & vioLED0) != 0U) {
-    if (signal & vioLED0) {
+    if ((signal & vioLED0) != 0U) {
       BSP_LED_On(LED_RED);
     } else {
       BSP_LED_Off(LED_RED);
@@ -441,7 +441,7 @@ void vioSetSignal (uint32_t mask, uint32_t signal) {
   }
 
   if ((mask & vioLED1) != 0U) {
-    if (signal & vioLED1) {
+    if ((signal & vioLED1) != 0U) {
       BSP_LED_On(LED_GREEN);
     } else {
       BSP_LED_Off(LED_GREEN);
--- gen_pack.sh
+++ gen_pack.sh
@@ -66,7 +66,6 @@ PACK_DIRS=${PACK_DIRS//$PACK_WAREHOUSE/}
   
 # Specify file names to be added to pack base directory
 PACK_BASE_FILES="
-  LICENSE
 "
 
 ############ DO NOT EDIT BELOW ###########
